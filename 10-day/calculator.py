cb4131c727eba2e2fbacd8a62cb5b396effdcfcf
Valentin L <valik2105@mail.ru>
2023-07-06 15:05:36 +0300

    Day 10 complete

---
+++ 10-day/calculator.py
@@ -0,0 +1,49 @@
+# This is a calculator
+from art import logo
+
+# Add
+def add(n1, n2):
+    return n1 + n2
+
+# Subtract
+def subtract(n1, n2):
+    return n1 - n2
+
+# Multiply
+def multiply(n1, n2):
+    return n1 * n2
+
+# Devide
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+def calculator():
+    print(logo)
+
+    num1 = float(input("What's the first number?: "))
+    for operator in operations:
+        print(operator)
+    should_continue = True
+
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation.: ")
+        if choice == 'y':
+            num1 = answer
+        else:
+            calculator()
+
+calculator()
